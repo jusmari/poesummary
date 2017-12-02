3b4f4b0874f716c6463d13e6b53e16189839006a
Jussi Maristo <maot@cs.helsinki.fi>
2017-12-02 11:01:10 +0200

    getting rid of service worker

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 import { unregister } from './registerServiceWorker';
 unregister();
+
+ReactDOM.render(<App />, document.getElementById('root'));
+//registerServiceWorker();
